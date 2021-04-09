f9bd8b61c81dd6edc5ed777c4ac6227a537e992f
hunterSherouse <huntersherouse2@gmail.com>
2021-04-09 13:58:57 -0500

    initial commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        //methods:
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool z)
+        {
+            if(z == true)
+            {
+                var result = x + y;
+                if (result == 1)
+                { 
+                    return ($"{result} dollar."); 
+                }
+                else
+                {
+                    return ($"{result} dollars.");
+                }
+
+            }
+
+            else
+            {
+                var result = x + y;
+                return ($"{result}");
+            }
+
+        }
+
+    }
+
+
+
+
+    
+}
+
+
